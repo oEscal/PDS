0f4811300bcc143f565d2e2d31f95b8ade6b3172
oEscal <pedroescaleira@hotmail.com>
2019-02-24 22:53:15 +0000

    functions in SoapGenerator created, but not implemented

---
+++ pratica1/src/SoapGenerator.java
@@ -0,0 +1,41 @@
+import java.util.List;
+import java.util.Map;
+
+public class SoapGenerator {
+
+    private static final int MAX_SIZE = 60;
+
+    public static List<List<Character>> generate(List<String> words){
+
+
+
+        return null;
+    }
+
+    private static void insertRandom(List<List<Character>> current_soap,
+                                     Map<Character, List<Integer[]>> positions){
+
+
+    }
+
+    private static void insertWithIntersection(List<List<Character>> current_soap,
+                                               Map<Character, List<Integer[]>> positions){
+
+
+
+    }
+
+    private static void resizeSoap(List<List<Character>> current_soap, int min_size){
+
+
+
+    }
+
+    private static boolean generateRandomDirection(List<List<Character>> current_soap,
+                                                   int possible_directions, int position){
+
+
+        return false;
+    }
+
+}
