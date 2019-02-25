c4c8604d322375f6afe31b6588026599e15a3039
oEscal <pedroescaleira@hotmail.com>
2019-02-25 00:46:19 +0000

    insertRandom generatePossiblePositions

--- pratica1/src/SoapGenerator.java
+++ pratica1/src/SoapGenerator.java
@@ -1,5 +1,7 @@
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class SoapGenerator {
 
@@ -9,33 +11,65 @@ public class SoapGenerator {
 
 
 
+        int[][] possible_diretions = generatePossiblePositions();
+
         return null;
     }
 
     private static void insertRandom(List<List<Character>> current_soap,
-                                     Map<Character, List<Integer[]>> positions){
+                                     List<Integer[]> possible_positions,
+                                     Map<Character, List<Integer[]>> positions,
+                                     String word, int[][] possible_directions){
 
+        boolean try_new_position;
+        Integer[] position_to_insert;
 
-    }
+        //do{
+        //    Random rand = new Random();
+        //    position_to_insert = possible_positions.get(rand.nextInt(possible_positions.size()));
+//
+        //    try_new_position = generateRandomDirection(current_soap, word, possible_directions, position_to_insert);
+        //}while(try_new_position);
+//
+        //possible_positions.remove(position_to_insert);
+//
+        }
 
     private static void insertWithIntersection(List<List<Character>> current_soap,
-                                               Map<Character, List<Integer[]>> positions){
-
+                                               Map<Character, List<Integer[]>> positions,
+                                               String word){
 
 
     }
 
-    private static void resizeSoap(List<List<Character>> current_soap, int min_size){
+    private static void resizeSoap(List<List<Character>> current_soap,
+                                   List<Integer[]> possible_positions,
+                                   String word, int min_size){
 
 
 
     }
 
     private static boolean generateRandomDirection(List<List<Character>> current_soap,
-                                                   int possible_directions, int position){
+                                                   String word, int[] possible_directions, Integer[] position){
 
 
         return false;
+    }
+
+    private static int[][] generatePossiblePositions(){
+
+        int[][] possible_directions = new int[8][2];
+        int[] coordinate__possible_values = {-1, 0, 1};
+        int indexes = 0;
+        for(int n1 : coordinate__possible_values)
+            for(int n2 : coordinate__possible_values){
+                possible_directions[indexes][0] = n1;
+                if(n1 != 0 || n2 != 0)
+                    possible_directions[indexes++][1] = n2;
+            }
+
+        return possible_directions;
     }
 
 }
