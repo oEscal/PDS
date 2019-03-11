ce6b9e0f6ebedd4576578fdb472ea8e09c6eb92d
andrebaiao <andre.alves@ua.pt>
2019-03-11 20:31:20 +0000

    improved code structure

--- aula03/lab3.java
+++ aula03/lab3.java
@@ -13,12 +13,91 @@ import java.util.*;
 
 public class lab3 {
 
+    //Initialize global variables
+    private static final int  MIN_NUMBER_DOOR = 1;
     static Scanner keyBoard = new Scanner(System.in);
 
     public static void main(String args[]){
 
+        List <Member> street = new ArrayList<>();
         while(true)
-            menu();
+            menu(street);
+
+    }
+
+    private static void menu(List <Member> street){
+
+        System.out.print("Load, Map, Add, Remove, List, LookUp, Clear, Quit\nCommand: ");
+        String[] option = keyBoard.nextLine().split("[\\s]+");
+
+        switch (option[0].toLowerCase()){
+            case "load":
+                List<String> content = readFileLines(option[1]);
+                getFamilies(content , street);
+                break;
+            case "map":
+                map(street);
+                break;
+            case "add":
+                addMember(option,street);
+                break;
+            case "remove":
+                removeMember(option[1],street);
+                break;
+            case "list":
+                break;
+            case "lookup":
+                showFamily(option[1],street);
+                break;
+            case "clear":
+                clearResidents(street);
+                break;
+            case "quit":
+                System.exit(1);
+                break;
+        }
+
+    }
+
+    /*
+    Function to read files
+    */
+    private static List<String> readFileLines(String file_name) {
+
+        Path path_file = Paths.get(file_name);
+        List<String> file_lines = new ArrayList<>();
+
+        try {
+            Files.lines(path_file).forEach(line -> file_lines.add(line));
+        }catch(IOException e) {
+            System.out.println("Error in file opening!");
+            System.exit(1);
+        }
+
+        return file_lines;
+    }
+
+    /*
+    Function to put families in data structure street
+    */
+    private static void getFamilies(List <String> content, List <Member> street){
+
+        int x1,x2;
+        content.remove(0);
+
+        for (String member : content){
+
+            String[] member_parts = member.split("[-\\s]+");
+
+            x1 = Integer.parseInt(member_parts[0]);
+            x2 = Integer.parseInt(member_parts[1]);
+            checkDoors(x1,x2);
+
+            if ( street.contains(Member.factory(member_parts[2], x1, x2)) )
+                System.err.println("Resident already exist!");
+            else
+                street.add( Member.factory(member_parts[2], x1, x2) );
+        }
 
     }
 
@@ -58,81 +137,11 @@ public class lab3 {
         }
     }
 
-    private static boolean contains(TreeSet<Member> set, Member member_to_check){
-        AtomicBoolean result = new AtomicBoolean(false);
-        set.forEach(item -> {
-            if(item.equals(member_to_check))
-                result.set(true);
-        });
-        return result.get();
-    }
-
-    private static String generateWhiteString(int size){
-        String result = "";
-        for(int n = 0; n < size; n++)
-            result += " ";
-        return result;
-    }
-
-
-    //Data Structures
-    private static final int  MIN_NUMBER_DOOR = 1;
-    private static List <Member> street = new ArrayList<>();
-
-
-    /*
-    Function to read files
-    */
-    private static List<String> readFileLines(String file_name) {
-
-        Path path_file = Paths.get(file_name);
-        List<String> file_lines = new ArrayList<>();
-
-        try {
-            Files.lines(path_file).forEach(line -> file_lines.add(line));
-        }catch(IOException e) {
-            System.out.println("Error in file opening!");
-            System.exit(1);
-        }
-
-        return file_lines;
-    }
-
-
-    private static void checkDoors(int x1 , int x2){
-        if ( x1 < MIN_NUMBER_DOOR || x2 < MIN_NUMBER_DOOR ) ErrorStreet.nonExistDoorError();
-        if ( x1 > x2 ) ErrorStreet.errDoorsIntervalError();
-    }
-
-    /*
-    Function to put families in data structure street
-    */
-    private static void getFamilies(List <String> content){
-
-        int x1,x2;
-        content.remove(0);
-
-        for (String member : content){
-
-            String[] member_parts = member.split("[-\\s]+");
-
-            x1 = Integer.parseInt(member_parts[0]);
-            x2 = Integer.parseInt(member_parts[1]);
-            checkDoors(x1,x2);
-
-            if ( street.contains(Member.factory(member_parts[2], x1, x2)) )
-                System.err.println("Resident already exist!");
-            else
-                street.add( Member.factory(member_parts[2], x1, x2) );
-        }
-
-    }
-
     /*
     Function to add resident on street
     If resident exist already, send error
      */
-    private static void addMember(String[] option){
+    private static void addMember(String[] option , List <Member> street){
 
         int x1,x2;
 
@@ -145,14 +154,13 @@ public class lab3 {
             System.err.println("Numbers of door not int!");
         }
 
-
     }
 
     /*
     Function to remove resident
     If resident don't exist, give error
      */
-    private static void removeMember(String name){
+    private static void removeMember(String name , List <Member> street){
 
         boolean checkExistence = true;
 
@@ -171,7 +179,7 @@ public class lab3 {
     /*
     Function to find family from one name
      */
-    private static void showFamily(String nameMember){
+    private static void showFamily(String nameMember, List <Member> street){
 
         int num_initial,num_final;
 
@@ -193,42 +201,30 @@ public class lab3 {
 
     }
 
-    private static void clearResidents(){
+    private static void clearResidents(List <Member> street){
         street.clear();
     }
 
-    private static void menu(){
-
-        System.out.print("Load, Map, Add, Remove, List, LookUp, Clear, Quit\nCommand: ");
-        String[] option = keyBoard.nextLine().split("[\\s]+");
-
-        switch (option[0].toLowerCase()){
-            case "load":
-                List<String> content = readFileLines(option[1]);
-                getFamilies(content);
-                break;
-            case "map":
-                map(street);
-                break;
-            case "add":
-                addMember(option);
-                break;
-            case "remove":
-                removeMember(option[1]);
-                break;
-            case "list":
-                break;
-            case "lookup":
-                showFamily(option[1]);
-                break;
-            case "clear":
-                clearResidents();
-                break;
-            case "quit":
-                System.exit(1);
-                break;
-        }
-
+    private static void checkDoors(int x1 , int x2){
+        if ( x1 < MIN_NUMBER_DOOR || x2 < MIN_NUMBER_DOOR ) ErrorStreet.nonExistDoorError();
+        if ( x1 > x2 ) ErrorStreet.errDoorsIntervalError();
     }
+
+    private static boolean contains(TreeSet<Member> set, Member member_to_check){
+        AtomicBoolean result = new AtomicBoolean(false);
+        set.forEach(item -> {
+            if(item.equals(member_to_check))
+                result.set(true);
+        });
+        return result.get();
+    }
+
+    private static String generateWhiteString(int size){
+        String result = "";
+        for(int n = 0; n < size; n++)
+            result += " ";
+        return result;
+    }
+
 
 }
