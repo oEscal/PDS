4ff55c11a2bbd537ae6f1b09b59498f1996f6cde
oEscal <pedroescaleira@hotmail.com>
2019-04-01 10:11:15 +0100

    classes coppyed from pdf

---
+++ aula06/Ex1/Database.java
@@ -0,0 +1,25 @@
+package aula06.Ex1;
+
+import java.util.Vector;
+
+public class Database {
+
+    private Vector<Employee> employees; // Stores the employees
+
+    public Database() {
+        employees = new Vector<>();
+    }
+
+
+    public boolean addEmployee(Employee employee) {
+
+    }
+
+    public void deleteEmployee(long emp_num) {
+
+    }
+
+    public Employee[] getAllEmployees() {
+
+    }
+}
---
+++ aula06/Ex1/Registos.java
@@ -0,0 +1,31 @@
+package aula06.Ex1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Registos {
+
+    // Data elements
+    private ArrayList<Empregado> empregados; // Stores the employees
+
+    public Registos() {
+        empregados = new ArrayList<>();
+    }
+
+
+    public void insere(Empregado emp) {
+// Code to insert employee
+    }
+
+    public void remove(int codigo) {
+// Code to remove employee
+    }
+
+    public boolean isEmpregado(int codigo) {
+// Code to find employee
+    }
+
+    public List<Empregado> listaDeEmpregados() {
+// Code to retrieve collection
+    }
+}
