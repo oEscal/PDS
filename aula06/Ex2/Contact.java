0bad5b414f503a09e4ef0611fd3c59aa1206be48
andrebaiao <andre.alves@ua.pt>
2019-04-01 21:05:22 +0100

    inicio de estrutura de codigo ex2

---
+++ aula06/Ex2/Contact.java
@@ -0,0 +1,15 @@
+package aula06.Ex2;
+
+public class Contact {
+
+    private String nome;
+    private int numero;
+
+    public Contact(String nome, int numero){
+        this.nome=nome;
+        this.numero=numero;
+    }
+
+
+
+}
