3cec5547ccd3d62ea874aa2aad626b930e0c91ec
andrebaiao <andre.alves@ua.pt>
2019-04-02 21:44:57 +0100

    implement test function

--- aula06/Ex2/Test.java
+++ aula06/Ex2/Test.java
@@ -7,9 +7,17 @@ public class Test {
         Contacts list_contacts = new Contacts();
         list_contacts.openAndLoad(new ContactsStorage("contacts", FileType.bin));
 
+        list_contacts.add(new Contact("Best mae", 9174234));
         list_contacts.add(new Contact("Escaleira", 911));
         list_contacts.add(new Contact("Escaleira", 910));
         list_contacts.add(new Contact("Baião", 933));
+
+        Contact mae = list_contacts.getByName("Best mae");
+        System.out.println(list_contacts.exist(mae));
+        list_contacts.remove(mae);
+        System.out.println(list_contacts.exist(mae));
+        System.out.println(list_contacts.getByName("Best mae"));
+
         list_contacts.saveAndClose(new ContactsStorage("contacts", FileType.bin));
     }
 
