dd713bc65a2baeaf9c24e34261dba635938083a9
andrebaiao <andre.alves@ua.pt>
2019-05-20 09:40:25 +0100

    initialized identities

--- aula10/ex01/Cliente.java
+++ aula10/ex01/Cliente.java
@@ -1,4 +1,10 @@
 package aula10.ex01;
 
 public class Cliente {
+
+    private String name;
+
+    public Cliente(String name){
+        this.name = name;
+    }
 }
--- aula10/ex01/Produto.java
+++ aula10/ex01/Produto.java
@@ -1,4 +1,16 @@
 package aula10.ex01;
 
 public class Produto {
+
+    static int id = 0;
+    private int id_product = id + 1;
+
+    private String description;
+    private double price;
+
+    public Produto (String description, double price){
+        this.description = description;
+        this.price = price;
+    }
+
 }
