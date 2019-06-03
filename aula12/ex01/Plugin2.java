2593e55422e1339924aef443ac74b1e478f5d900
andrebaiao <andre.alves@ua.pt>
2019-06-03 10:13:20 +0100

    ex01

---
+++ aula12/ex01/Plugin2.java
@@ -0,0 +1,9 @@
+package aula12.ex01;
+
+public class Plugin2 implements IPlugin {
+
+    @Override
+    public void metodo() {
+        System.out.println("Plugin2: metodo invocado");
+    }
+}
---
+++ aula12/ex01/Plugin3.java
@@ -0,0 +1,9 @@
+package aula12.ex01;
+
+public class Plugin3 implements IPlugin {
+
+    @Override
+    public void metodo() {
+        System.out.println("Plugin3 : metodo invocado!");
+    }
+}
